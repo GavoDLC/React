672e3865d6d80778017a0a4d97c13b41a5aed3d8
GavoDLC <gabriel_apa2@hotmail.com>
2024-02-29 20:06:59 -0500

    Se creo la ruta  de RouterProvider

--- src/components/ShowHide.jsx
+++ src/components/ShowHide.jsx
@@ -2,12 +2,15 @@ import React, {useState} from "react";
 function ShowHide() {
     //hook de react
     const [show, setShow]=useState(true);
+    //evento de click
     const handleClick = (event) => {
         setShow(!show);
     }
 
   return <div>
-  <button onClick={handleClick}>Hide Text</button>
+    {/* operador ternario */}
+    {/* onClick es un evento de clic */}
+  <button onClick={handleClick}>{show ? "Hide Text" : "Show Text"}</button>
     {show && <h2> HIDE ME!</h2>}
   </div>
   
