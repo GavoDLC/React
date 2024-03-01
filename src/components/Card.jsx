98f37603034569fb5eda684de11dcc208e2b37ea
GavoDLC <gabriel_apa2@hotmail.com>
2024-02-29 20:45:10 -0500

    Se crearon los enlaces de las cards a las vistas y se dio estilo css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import "./Card.css";
+import { Link } from "react-router-dom";
 function Card({title="titulo por defecto", description="descripción por defecto"}) {
   return (
     <div className="Card">
+        <Link to={title}>
         <h2>{title}</h2>
+        </Link>
         <p>{description}</p>
     </div>
   )
--- src/views/VehicleView.jsx
+++ src/views/VehicleView.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './VehicleView.css'
 
 function VehicleView({vehicle}) {
   return (
 
-    <div>
+    <div className='VehicleView'>
         <h1>{vehicle.name}</h1>
         <h2>{vehicle.description}</h2>
         <img src={vehicle.image} alt={vehicle.name,"image"} />
